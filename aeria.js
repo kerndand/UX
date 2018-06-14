2051cc8d4d4fedb97e6ab7b626886f3e8b4ce352
danie <danie@DanielLaptop>
2018-06-14 18:28:12 +0200

    aeria Prototyp erste Version

--- aeria.js
+++ aeria.js
@@ -1,1 +1,30 @@
+var aeria;
+(function (aeria) {
+    window.addEventListener("load", init);
+    function init(event) {
+        loadingScreen();
+    }
+    function loadingScreen() {
+        let loadingScreen = document.createElement("div");
+        loadingScreen.setAttribute("id", "loadingScreen");
+        loadingScreen.innerHTML = "<p>aeria</p>";
+        loadingScreen.innerHTML += "<p>Kitesport. Worldwide.</p>";
+        document.getElementById("background").appendChild(loadingScreen);
+        window.setTimeout(searchScreen, 2000);
+    }
+    function searchScreen() {
+        document.getElementById("loadingScreen").style.display = "none";
+        let search = document.createElement("input");
+        search.setAttribute("type", "text");
+        search.setAttribute("placeholder", "Zielort eingeben");
+        search.setAttribute("name", "search");
+        let searchButton = document.createElement("button");
+        searchButton.innerHTML = "<i" + " class=" + "'fa fa-search fa'" + "></i>";
+        let searchScreen = document.createElement("div");
+        searchScreen.setAttribute("id", "searchScreen");
+        searchScreen.appendChild(search);
+        searchScreen.appendChild(searchButton);
+        document.getElementById("background").appendChild(searchScreen);
+    }
+})(aeria || (aeria = {}));
 //# sourceMappingURL=aeria.js.map
---
+++ aeria.ts
@@ -0,0 +1,36 @@
+namespace aeria {
+    window.addEventListener("load", init);
+
+    function init(event: Event): void {
+        loadingScreen();
+    }
+
+    function loadingScreen(): void {
+        let loadingScreen: HTMLDivElement = document.createElement("div");
+        loadingScreen.setAttribute("id", "loadingScreen");
+        loadingScreen.innerHTML = "<p>aeria</p>";
+        loadingScreen.innerHTML += "<p>Kitesport. Worldwide.</p>";
+        document.getElementById("background").appendChild(loadingScreen);
+        window.setTimeout(searchScreen, 2000);
+    }
+
+    function searchScreen(): void {
+        document.getElementById("loadingScreen").style.display = "none";
+
+        let search: HTMLInputElement = document.createElement("input");
+        search.setAttribute("type", "text");
+        search.setAttribute("placeholder", "Zielort eingeben");
+        search.setAttribute("name", "search");
+
+        let searchButton: HTMLButtonElement = document.createElement("button");
+        searchButton.innerHTML = "<i" + " class=" + "'fa fa-search fa'" + "></i>";
+
+        let searchScreen: HTMLDivElement = document.createElement("div");
+        searchScreen.setAttribute("id", "searchScreen");
+        searchScreen.appendChild(search);
+        searchScreen.appendChild(searchButton);
+
+        document.getElementById("background").appendChild(searchScreen);
+    }
+
+}
