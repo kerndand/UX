cd3b1b7ad6724cd6c98c3c678a3af6b944048e22
danie <danie@DanielLaptop.hfu-open.fu.hs-furtwangen.de>
2018-06-21 15:31:55 +0200

    porto

--- aeria.js
+++ aeria.js
@@ -1,6 +1,7 @@
 var aeria;
-(function (aeria) {
+(function (aeria_1) {
     window.addEventListener("load", init);
+    let aeria = [];
     function init(event) {
         loadingScreen();
     }
@@ -29,51 +30,100 @@ var aeria;
         searchScreen.appendChild(searchButton);
         document.getElementById("background").appendChild(searchScreen);
         document.getElementsByTagName("button")[0].addEventListener("click", resultScreen);
+        aeria.push(searchScreen);
     }
     function resultScreen() {
         let input = document.getElementById("search");
         if (input.value == "Porto" || input.value == "porto") {
-            document.getElementById("searchScreen").style.display = "none";
-            let map = document.createElement("img");
-            map.setAttribute("src", "Karte.png");
-            let resultScreen = document.createElement("div");
-            resultScreen.setAttribute("id", "resultScreen");
-            resultScreen.innerHTML = "<span" + " class=" + "'fa fa-map-marker fa-2x'" + "></span>";
-            resultScreen.innerHTML += "<span" + " class=" + "'fa fa-map-marker fa-2x'" + "></span>";
-            resultScreen.appendChild(map);
-            document.getElementById("background").appendChild(resultScreen);
-            let icon = document.getElementsByTagName("span");
-            icon[0].addEventListener("click", placeScreen);
+            if (aeria.length == 1) {
+                document.getElementById("searchScreen").style.display = "none";
+                let map = document.createElement("img");
+                map.setAttribute("src", "Karte.png");
+                let resultScreen = document.createElement("div");
+                resultScreen.setAttribute("id", "resultScreen");
+                resultScreen.innerHTML = "<span" + " class=" + "'fa fa-map-marker fa-2x'" + "></span>";
+                resultScreen.innerHTML += "<span" + " class=" + "'fa fa-map-marker fa-2x'" + "></span>";
+                resultScreen.appendChild(map);
+                let backButton = document.createElement("button");
+                backButton.innerHTML = "<i" + " class=" + "'fa fa-chevron-left fa-2x'" + "></i>";
+                resultScreen.appendChild(backButton);
+                document.getElementById("background").appendChild(resultScreen);
+                let icon = document.getElementsByTagName("span");
+                icon[0].addEventListener("click", placeScreen);
+                backButton.addEventListener("click", back);
+                aeria.push(resultScreen);
+                console.log(aeria);
+            }
+            else {
+                document.getElementById("resultScreen").style.display = "block";
+                document.getElementById("searchScreen").style.display = "none";
+            }
         }
         else {
             alert("Diesen Ort gibt es leider in diesem Prototypen nicht. Bitte versuchen Sie es erneut!");
         }
-        function placeScreen() {
+        function back() {
             document.getElementById("resultScreen").style.display = "none";
-            let placeDiv = document.createElement("div");
-            placeDiv.setAttribute("id", "placeDiv");
-            placeDiv.innerHTML = "<p>Av. do Brasil 816</p>";
-            placeDiv.innerHTML += "<p>4150-154 Porto</p>";
-            placeDiv.innerHTML += "<p>Portugal</p>";
-            let infoButton = document.createElement("button");
-            infoButton.innerHTML = "<i" + " class=" + "'fa fa-chevron-right fa-2x'" + "></i>";
-            placeDiv.appendChild(infoButton);
-            infoButton.addEventListener("click", infoScreen);
-            document.getElementById("background").appendChild(placeDiv);
+            document.getElementById("searchScreen").style.display = "block";
+        }
+        function placeScreen() {
+            if (aeria.length == 2) {
+                document.getElementById("resultScreen").style.display = "none";
+                let placeDiv = document.createElement("div");
+                placeDiv.setAttribute("id", "placeDiv");
+                placeDiv.innerHTML = "<p>Av. do Brasil 816</p>";
+                placeDiv.innerHTML += "<p>4150-154 Porto</p>";
+                placeDiv.innerHTML += "<p>Portugal</p>";
+                let backButton1 = document.createElement("button");
+                backButton1.setAttribute("id", "back");
+                backButton1.innerHTML = "<i" + " class=" + "'fa fa-chevron-left'" + "></i>";
+                placeDiv.appendChild(backButton1);
+                let infoButton = document.createElement("button");
+                infoButton.setAttribute("id", "info");
+                infoButton.innerHTML = "<i" + " class=" + "'fa fa-chevron-right'" + "></i>";
+                placeDiv.appendChild(infoButton);
+                infoButton.addEventListener("click", infoScreen);
+                backButton1.addEventListener("click", back2);
+                document.getElementById("background").appendChild(placeDiv);
+                aeria.push(placeDiv);
+            }
+            else {
+                document.getElementById("resultScreen").style.display = "none";
+                document.getElementById("placeDiv").style.display = "block";
+            }
+        }
+        function back2() {
+            document.getElementById("resultScreen").style.display = "block";
+            document.getElementById("placeDiv").style.display = "none";
         }
         function infoScreen() {
-            document.getElementById("placeDiv").style.display = "none";
-            let infoScreen = document.createElement("div");
-            infoScreen.setAttribute("id", "infoScreen");
-            infoScreen.innerHTML = "<h3>Surfivor Surfschool - Porto</h3>";
-            infoScreen.innerHTML += "<span" + " class=" + "'fa fa-tachometer fa-2x'" + "></span>" + "<p> 4Bft (11 - 16 kn)</p>";
-            infoScreen.innerHTML += "<span" + " class=" + "'fa fa-compass fa-2x'" + "></span>" + "<p> Off - Shore (Ablandiger Wind)</p>";
-            infoScreen.innerHTML += "<span" + " class=" + "'fa fa-cloud fa-2x'" + "></span>" + "<p> Heiter mit leichten B&oumlhen, 25&degC</p>";
-            infoScreen.innerHTML += "<span" + " class=" + "'fa fa-low-vision fa-2x'" + "></span>" + "<p>Klare Sicht, ca. 7,2km</p>";
-            infoScreen.innerHTML += "<span" + " class=" + "'material-icons'" + ">beach_access</span>" + "<p>Wahrscheinlichkeit 45%, Menge 5mm</p>";
-            infoScreen.innerHTML += "<span" + " class=" + "'glyphicon glyphicon-tint'" + "></span>" + "<p>92%</p>";
-            infoScreen.innerHTML += "<i" + " class=" + "'fa fa-square-o fa'" + "><p> Anf&aumlnger</p></i>" + " <i" + " class=" + "'fa fa-check-square-o'" + "><p> Fortgeschrittener</p></i>" + " <i" + " class=" + "'fa fa-check-square-o'" + "><p> Profi</p></i>";
-            document.getElementById("background").appendChild(infoScreen);
+            if (aeria.length == 3) {
+                document.getElementById("placeDiv").style.display = "none";
+                let infoScreen = document.createElement("div");
+                infoScreen.setAttribute("id", "infoScreen");
+                infoScreen.innerHTML = "<h3>Surfivor Surfschool - Porto</h3>";
+                infoScreen.innerHTML += "<span" + " class=" + "'fa fa-tachometer fa-2x'" + "></span>" + "<p> 4Bft (11 - 16 kn)</p>";
+                infoScreen.innerHTML += "<span" + " class=" + "'fa fa-compass fa-2x'" + "></span>" + "<p> Off - Shore (Ablandiger Wind)</p>";
+                infoScreen.innerHTML += "<span" + " class=" + "'fa fa-cloud fa-2x'" + "></span>" + "<p> Heiter mit leichten B&oumlhen, 25&degC</p>";
+                infoScreen.innerHTML += "<span" + " class=" + "'fa fa-low-vision fa-2x'" + "></span>" + "<p>Klare Sicht, ca. 7,2km</p>";
+                infoScreen.innerHTML += "<span" + " class=" + "'material-icons'" + ">beach_access</span>" + "<p>Wahrscheinlichkeit 45%, Menge 5mm</p>";
+                infoScreen.innerHTML += "<span" + " class=" + "'glyphicon glyphicon-tint'" + "></span>" + "<p>92%</p>";
+                infoScreen.innerHTML += "<i" + " class=" + "'fa fa-square-o fa'" + "><p> Anf&aumlnger</p></i>" + " <i" + " class=" + "'fa fa-check-square-o'" + "><p> Fortgeschrittener</p></i>" + " <i" + " class=" + "'fa fa-check-square-o'" + "><p> Profi</p></i>";
+                let backButton2 = document.createElement("button");
+                backButton2.innerHTML = "<i" + " class=" + "'fa fa-chevron-left fa-2x'" + "></i>";
+                infoScreen.appendChild(backButton2);
+                backButton2.addEventListener("click", back3);
+                document.getElementById("background").appendChild(infoScreen);
+                aeria.push(infoScreen);
+            }
+            else {
+                document.getElementById("placeDiv").style.display = "none";
+                document.getElementById("infoScreen").style.display = "block";
+            }
+        }
+        function back3() {
+            document.getElementById("placeDiv").style.display = "block";
+            document.getElementById("infoScreen").style.display = "none";
         }
     }
 })(aeria || (aeria = {}));
